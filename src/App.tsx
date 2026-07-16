b16c8f8a2fa223532b56e8890419e129d74c5a63
Maverick Mahbub <maverickmahbub.official@gmail.com>
2026-07-16 22:40:01 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5144,8 +5144,8 @@ export default function App() {
             </div>
           </div>
           </div>
+      );
       }
-      )
       
       const handleSynthesizeAndPlay = async () => { if (!scriptText) {
    alert("Please enter script first");
